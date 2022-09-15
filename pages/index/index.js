cfa50a1dc26fa28c4378d5554626d84a96137640
bobostudio <m18576493832@163.com>
2022-09-15 17:27:49 +0800

    feat: init tally miniprogram

---
+++ pages/index/index.js
@@ -0,0 +1,20 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: '',
+  },
+  // 事件处理函数
+  bindViewTap() {
+    ft.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+    this.setData({
+      motto: 'hello world'
+    })
+  }
+})
